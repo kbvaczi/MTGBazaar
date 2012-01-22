ebd726819daea50e1bd02908690b1fa90c9e5e4e
Ken <kvaczi2@gmail.com>
2012-01-22 09:02:21 -0600

    test

--- app/models/mtg/mtg_card.rb
+++ app/models/mtg/mtg_card.rb
@@ -2,8 +2,3 @@ class MtgCard < ActiveRecord::Base
   belongs_to :set, :class_name => "MtgSet"
   
 end
-
-
-!(/\.(?!htaccess)[^/]*|\.(tmproj|o|pyc)|/Icon\r|/svn-commit(\.[2-9])?\.tmp)$
-
-!.*/(\.[^/]*|CVS|_darcs|_MTN|\{arch\}|blib|.*~\.nib|.*\.(framework|app|pbproj|pbxproj|xcode(proj)?|bundle))$
