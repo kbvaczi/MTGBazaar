5d218920fb78f2d75ad685fc52547e151b9b5f5f
Ken <kvaczi2@gmail.com>
2012-02-25 01:16:01 -0600

    removed legality fields from mtg_cards, added image_back_path for double-sided cards

--- db/migrate/20120122125236_create_mtg_cards.rb
+++ db/migrate/20120122125236_create_mtg_cards.rb
@@ -14,20 +14,21 @@ class CreateMtgCards < ActiveRecord::Migration
       t.string   :mana_cost,                 :default => "", :null => false
       t.string   :power,                     :default => "", :null => false
       t.string   :toughness,                 :default => "", :null => false
-      t.string   :legality_block,            :default => "", :null => false
-      t.string   :legality_standard,         :default => "", :null => false
-      t.string   :legality_extended,         :default => "", :null => false
-      t.string   :legality_modern,           :default => "", :null => false
-      t.string   :legality_legacy,           :default => "", :null => false
-      t.string   :legality_vintage,          :default => "", :null => false
-      t.string   :legality_highlander,       :default => "", :null => false
-      t.string   :legality_french_commander, :default => "", :null => false
-      t.string   :legality_commander,        :default => "", :null => false
-      t.string   :legality_peasant,          :default => "", :null => false
-      t.string   :legality_pauper,           :default => "", :null => false
+      #t.string   :legality_block,            :default => "", :null => false
+      #t.string   :legality_standard,         :default => "", :null => false
+      #t.string   :legality_extended,         :default => "", :null => false
+      #t.string   :legality_modern,           :default => "", :null => false
+      #t.string   :legality_legacy,           :default => "", :null => false
+      #t.string   :legality_vintage,          :default => "", :null => false
+      #t.string   :legality_highlander,       :default => "", :null => false
+      #t.string   :legality_french_commander, :default => "", :null => false
+      #t.string   :legality_commander,        :default => "", :null => false
+      #t.string   :legality_peasant,          :default => "", :null => false
+      #t.string   :legality_pauper,           :default => "", :null => false
 
       t.string   :multiverse_id,             :default => "", :null => false
       t.string   :image_path,                :default => "", :null => false
+      t.string   :image_back_path,           :default => "", :null => false
 
       t.timestamps
     end
@@ -36,13 +37,11 @@ class CreateMtgCards < ActiveRecord::Migration
     
     add_index :mtg_cards , :artist
     add_index :mtg_cards , :mana_color
-    add_index :mtg_cards , :mana_cost
     add_index :mtg_cards , :name
-    #add_index :mtg_cards , :power
-    #add_index :mtg_cards , :toughness
     add_index :mtg_cards , :rarity
     add_index :mtg_cards , :set_id
     add_index :mtg_cards , :card_type
+    add_index :mtg_cards , :card_subtype    
 
   end
   
