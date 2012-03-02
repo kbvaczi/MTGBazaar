36517c8e4360d61e7466a38c65c57f9d72d9d7da
Chris <Chris@mtgbazaar.com>
2012-03-02 15:36:07 -0600

    minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -83,49 +83,72 @@ ActiveRecord::Schema.define(:version => 20120302044426) do
 
   create_table "mtg_cards", :force => true do |t|
     t.integer  "set_id"
-    t.string   "name",            :default => "",   :null => false
-    t.string   "card_type",       :default => "",   :null => false
-    t.string   "card_subtype",    :default => "",   :null => false
-    t.string   "rarity",          :default => "",   :null => false
-    t.string   "artist",          :default => "",   :null => false
-    t.string   "description",     :default => "",   :null => false
-    t.string   "mana_string",     :default => "",   :null => false
-    t.string   "mana_color",      :default => "",   :null => false
-    t.string   "mana_cost",       :default => "",   :null => false
-    t.string   "power",           :default => "",   :null => false
-    t.string   "toughness",       :default => "",   :null => false
-    t.string   "multiverse_id",   :default => "",   :null => false
-    t.string   "image_path",      :default => "",   :null => false
-    t.string   "image_back_path", :default => "",   :null => false
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.string   "name",                      :default => "",   :null => false
+    t.string   "card_type",                 :default => "",   :null => false
+    t.string   "card_subtype",              :default => "",   :null => false
+    t.string   "rarity",                    :default => "",   :null => false
+    t.string   "artist",                    :default => "",   :null => false
+    t.string   "description",               :default => "",   :null => false
+    t.string   "mana_string",               :default => "",   :null => false
+    t.string   "mana_color",                :default => "",   :null => false
+    t.string   "mana_cost",                 :default => "",   :null => false
+    t.string   "power",                     :default => "",   :null => false
+    t.string   "toughness",                 :default => "",   :null => false
+    t.string   "legality_block",            :default => "",   :null => false
+    t.string   "legality_standard",         :default => "",   :null => false
+    t.string   "legality_extended",         :default => "",   :null => false
+    t.string   "legality_modern",           :default => "",   :null => false
+    t.string   "legality_legacy",           :default => "",   :null => false
+    t.string   "legality_vintage",          :default => "",   :null => false
+    t.string   "legality_highlander",       :default => "",   :null => false
+    t.string   "legality_french_commander", :default => "",   :null => false
+    t.string   "legality_commander",        :default => "",   :null => false
+    t.string   "legality_peasant",          :default => "",   :null => false
+    t.string   "legality_pauper",           :default => "",   :null => false
+    t.string   "multiverse_id",             :default => "",   :null => false
+    t.string   "image_path",                :default => "",   :null => false
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.string   "card_number"
-    t.boolean  "active",          :default => true, :null => false
+    t.boolean  "active",                    :default => true, :null => false
   end
 
   add_index "mtg_cards", ["active"], :name => "index_mtg_cards_on_active"
   add_index "mtg_cards", ["artist"], :name => "index_mtg_cards_on_artist"
-  add_index "mtg_cards", ["card_subtype"], :name => "index_mtg_cards_on_card_subtype"
   add_index "mtg_cards", ["card_type"], :name => "index_mtg_cards_on_card_type"
   add_index "mtg_cards", ["mana_color"], :name => "index_mtg_cards_on_mana_color"
+  add_index "mtg_cards", ["mana_cost"], :name => "index_mtg_cards_on_mana_cost"
   add_index "mtg_cards", ["name"], :name => "index_mtg_cards_on_name"
   add_index "mtg_cards", ["rarity"], :name => "index_mtg_cards_on_rarity"
   add_index "mtg_cards", ["set_id"], :name => "index_mtg_cards_on_set_id"
 
   create_table "mtg_sets", :force => true do |t|
     t.integer  "block_id"
-    t.string   "name",         :default => "",           :null => false
-    t.string   "code",         :default => "",           :null => false
-    t.date     "release_date", :default => '2012-02-29'
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.boolean  "active",       :default => false,        :null => false
+    t.string   "name",         :default => "",                    :null => false
+    t.string   "code",         :default => "",                    :null => false
+    t.datetime "release_date", :default => '2012-02-17 14:13:44'
+    t.datetime "created_at",                                      :null => false
+    t.datetime "updated_at",                                      :null => false
+    t.boolean  "active",       :default => false,                 :null => false
   end
 
   add_index "mtg_sets", ["active"], :name => "index_mtg_sets_on_active"
   add_index "mtg_sets", ["code"], :name => "index_mtg_sets_on_code"
   add_index "mtg_sets", ["name"], :name => "index_mtg_sets_on_name"
   add_index "mtg_sets", ["release_date"], :name => "index_mtg_sets_on_release_date"
+
+  create_table "test1s", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test2s", :force => true do |t|
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "transactions", :force => true do |t|
     t.integer  "test1_id"
@@ -174,5 +197,7 @@ ActiveRecord::Schema.define(:version => 20120302044426) do
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
   end
+
+  add_index "users_deposits", ["account_id"], :name => "index_users_deposits_on_account_id"
 
 end
