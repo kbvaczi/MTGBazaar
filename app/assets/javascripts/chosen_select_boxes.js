121643014eb6fa6864ea136250821c8ee48671c7
Ken <kvaczi2@gmail.com>
2012-05-02 06:53:56 -0500

    styled chosen boxes

--- app/assets/javascripts/chosen_select_boxes.js
+++ app/assets/javascripts/chosen_select_boxes.js
@@ -3,13 +3,8 @@ $(document).ready(function() {
 
   // Activates chosen select boxes with css tag .chzn-select
   // do this last so that other jquery scripts can update select inputs if necessary before turning htem into chosen boxes
-  //$(".chzn-select").chosen();
-//  $(".chzn-select").chosen({no_results_text: "No results:"});
   $(".chzn-select").chosen({allow_single_deselect: true, disable_search_threshold: 10, max_selected_options: 4});
-  $(".chzn-select").bind("liszt:maxselected", function () { alert("yelp"); });
-
-  // Custom function which removes the chzn search bar for select boxes with .chzn-nosearch tag
-  //$(".chzn-nosearch").parent().find(".chzn-search").hide();
+  //$(".chzn-select").bind("liszt:maxselected", function () { alert("yelp"); });
   
 });
 
