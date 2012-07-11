5c316089295ff11c5cc5aea0a6214a54b9bbef6c
Christopher Odorizzi <codorizzi@unknown040cced6fbaa.gateway.2wire.net>
2012-07-10 20:17:34 -0500

    fixed the JS

--- app/assets/javascripts/change_content.js
+++ app/assets/javascripts/change_content.js
@@ -2,15 +2,14 @@ $('html').addClass('js');
 
 $(function() {
     var $divs = $('div', '#container'),
-        all$divs = $.map($divs, function(div) {return $(div);});
         total = $divs.length,
         counter = 0,
  
         showDiv = function() {
             $divs.stop().hide();
-            all$divs[counter].show('fast');
+            $($divs[counter]).show();
             counter = (counter + 1) % total;
-            setTimeout(showDiv, 3000);
+            setTimeout(showDiv, 10000);
         };
     $divs.hide();
     showDiv();
