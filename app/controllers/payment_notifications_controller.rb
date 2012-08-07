fa8393ac08ce69e432afc159ea82438709ddfa31
Ken <kvaczi2@gmail.com>
2012-08-07 09:48:03 -0500

    skip production authenticate for paypal notifications

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,7 +1,10 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
+  skip_before_filter :production_authenticate
+  
   def create
     PaymentNotification.create!(:params => params, :account_balance_transfer_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
     render :nothing => true
   end
+  
 end
