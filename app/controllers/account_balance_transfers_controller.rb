016f382e00e9d15ed9cd61e1b3a46dec3a1c465a
Ken <kvaczi2@gmail.com>
2012-08-27 21:16:18 -0500

    test

--- app/controllers/account_balance_transfers_controller.rb
+++ app/controllers/account_balance_transfers_controller.rb
@@ -9,6 +9,7 @@ class AccountBalanceTransfersController < ApplicationController
     @transfers = current_user.account.balance_transfers.includes(:payment_notifications).order("account_balance_transfers.created_at DESC").page(params[:page]).per(13)
   end
   
+=begin
   def test_payment
     
     test_withdraw = AccountBalanceTransfer.new(:balance => 10.50 )
@@ -46,7 +47,8 @@ class AccountBalanceTransfersController < ApplicationController
     return
     
   end
-  
+=end  
+
   def new_account_deposit
     set_back_path
     if params[:account_balance_transfer].present?
@@ -66,10 +68,10 @@ class AccountBalanceTransfersController < ApplicationController
     @deposit.account_id = current_user.account.id
     @deposit.approved_at = @deposit.created_at
     if @deposit.save
+      puts "TEST!!!"
+      puts create_deposit_notification_url(:secret => "b4z44r2012!")
+      puts "END TEST!!!"
       redirect_to paypal_deposit_url(@deposit), :method => :post
-      #current_user.account.balance_transfers << @deposit #link this deposit to current user's account
-      #current_user.account.update_attribute(:balance, current_user.account.balance + @deposit.balance) #add deposit to user's balance
-      #redirect_to session[:return_to] || :back, :notice => "Deposit Sucessful..."
     else
       flash[:error] = "There were one or more errors while trying to process your request"
       render 'new_account_deposit'
@@ -92,8 +94,8 @@ class AccountBalanceTransfersController < ApplicationController
      
      @withdraw = AccountBalanceTransfer.new(params[:account_balance_transfer])
      @withdraw.transfer_type = "withdraw"      
-     @withdraw.current_sign_in_ip = request.remote_ip # log user's IP address     
-     current_user.account.balance_transfers << @withdraw # link this withdraw to current user's account     
+     @withdraw.current_sign_in_ip = request.remote_ip # log user's IP address
+     @withdraw.account_id = current_user.account.id  # link this withdraw to current user's account             
      if current_user.account.balance - @withdraw.balance < 0
        flash[:error] = "You have insufficient funds in your account"
        render 'new_account_withdraw'
@@ -129,7 +131,7 @@ class AccountBalanceTransfersController < ApplicationController
      :cmd => "_s-xclick",
      :encrypted => encrypt_for_paypal(values)
    }
-   "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+   "https://www.sandbox.paypal.com/cgi-bin/webscr?" + params.to_query
   end
 
   # computes paypal commission based on price in dollars
