cfafc4fdd8adb2ccf235c1d9ef97bd152f2b102e
Ken <kvaczi2@gmail.com>
2012-08-31 12:06:41 -0500

    changed admin password

--- db/migrate/20120211135824_devise_create_admin_users.rb
+++ db/migrate/20120211135824_devise_create_admin_users.rb
@@ -41,7 +41,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'admin@mtgbazaar.com', :password => 'password', :password_confirmation => 'password')
+    AdminUser.create!(:email => 'admin@mtgbazaar.com', :password => 'b4z44r2012!', :password_confirmation => 'b4z44r2012!')
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
