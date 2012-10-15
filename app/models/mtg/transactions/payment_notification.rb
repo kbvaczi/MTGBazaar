c3335f6014e7ef96e8f62f77323c72ed461f92a9
Ken <kvaczi2@gmail.com>
2012-10-15 01:45:35 -0500

    set table name for payment_notifications

--- app/models/mtg/transactions/payment_notification.rb
+++ app/models/mtg/transactions/payment_notification.rb
@@ -1,5 +1,6 @@
 class Mtg::Transactions::PaymentNotification < ActiveRecord::Base
-  
+  self.table_name = 'mtg_transactions_payment_notifications'    
+    
   belongs_to :payment,    :class_name => "Mtg::Transactions::Payment"
   serialize  :response
   
