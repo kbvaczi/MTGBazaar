6c89fdc3e3bb92ded32a0754ef6edbd317c8d2a3
Ken <kvaczi2@gmail.com>
2012-10-16 02:03:33 -0500

    fixed incorrect parameter in payment_notifcations controller

--- app/controllers/mtg/transactions/payment_notifications_controller.rb
+++ app/controllers/mtg/transactions/payment_notifications_controller.rb
@@ -4,7 +4,7 @@ class Mtg::Transactions::PaymentNotificationsController < ApplicationController
   
   def payment_notification
     transaction = Mtg::Transaction.find(params[:id])
-    if params[:key].decrypt(:key => transaction.payment.calculate_key) == PAYPAL_CONFIG[:secret] # verify our secret which changes with every payment
+    if params[:secret].decrypt(:key => transaction.payment.calculate_key) == PAYPAL_CONFIG[:secret] # verify our secret which changes with every payment
       transaction.payment.payment_notifications.create(:response => params, 
                                                        :status => params['status'].downcase, 
                                                        :paypal_transaction_id => params[:transaction]['0']['.id'] )
