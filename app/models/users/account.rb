54af5bfd50f0d55bc50ddec32bb8ac7d51d81350
Ken <kvaczi2@gmail.com>
2012-10-19 17:15:12 -0500

    misc changes

--- app/models/users/account.rb
+++ app/models/users/account.rb
@@ -23,7 +23,8 @@ class Account < ActiveRecord::Base
                         :zipcode,
                         :security_question,
                         :security_answer
-                        
+
+  validates_uniqueness_of :paypal_username, :case_sensitive => false                        
 
   validates             :state, :format => { :with => /\A[A-Z]{2}\z/, :message => "Please enter a valid State Abbreviation" }
 
