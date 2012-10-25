05809c4970db8da1f8d72400292b465e5c60476e
Ken <kvaczi2@gmail.com>
2012-10-25 12:40:18 -0500

    fixed shipping label create ajax

--- app/controllers/mtg/transactions/shipping_labels_controller.rb
+++ app/controllers/mtg/transactions/shipping_labels_controller.rb
@@ -25,10 +25,8 @@ class Mtg::Transactions::ShippingLabelsController < ApplicationController
     end
     
     respond_to do |format|
-      format.html do 
-        redirect_to @label.params[:url]
-      end
-      format.js  { }
+      format.html { redirect_to @label.params[:url] } 
+      format.js  { current_transaction.reload }
     end
   end
   
