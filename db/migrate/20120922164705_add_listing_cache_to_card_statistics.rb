4ae0c96f5df62b58bf63185a862a228f6e778c45
Ken <kvaczi2@gmail.com>
2012-10-31 09:40:24 -0500

    misc

--- db/migrate/20120922164705_add_listing_cache_to_card_statistics.rb
+++ db/migrate/20120922164705_add_listing_cache_to_card_statistics.rb
@@ -1,7 +1,6 @@
 class AddListingCacheToCardStatistics < ActiveRecord::Migration
   def change
-    add_column :mtg_card_statistics, :price_min,      :integer
-    add_column :mtg_card_statistics, :listings_available, :integer    
-    
+    add_column :mtg_card_statistics, :price_min,          :integer,           :default => 0
+    add_column :mtg_card_statistics, :listings_available, :integer,           :default => 0    
   end
 end
