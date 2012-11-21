b3387b1a48e3003defb6f8f6ebe29398981c8c7a
Ken <kvaczi2@gmail.com>
2012-11-21 17:48:28 -0600

    testing http streaming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  stream
   helper :all
 
   before_filter :staging_authenticate        # simple HTTP authentication for production (TEMPORARY)
