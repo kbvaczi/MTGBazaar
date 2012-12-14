3413ca3fb5316c9dc344e2e9a5366b454689eaa3
Ken <kvaczi2@gmail.com>
2012-12-14 12:24:03 -0600

    updated show_product_name

--- app/helpers/mtg/products_helper.rb
+++ app/helpers/mtg/products_helper.rb
@@ -2,19 +2,32 @@
 # make this module available to be included in other areas by using "include Mtg::CardsHelper"
 module Mtg::ProductsHelper
 
-  def product_name(product)
+  def product_name(product, truncate = false)
+    product_type = ""
+    product_name = ""
     if product.class == Mtg::Card
-      product.name
+      product_name = product.name
     elsif product.class == Mtg::Cards::Listing || product.class == Mtg::Transactions::Item
-      if product.playset
-        "Playset:<br/>#{product.card.name}".html_safe
-      else
-        display_name(product.card.name)
-      end
+      product_type = "Playset" if product.playset
+      product_name = product.card.name
     elsif product.class == Mtg::Sets::Listing
-      "Full Set: <br/>".html_safe + product.set.name
+      product_type = "Full Set"
+      product_name = product.set.name
     else
-      "unknown"
+      product_name = "Unknown"
+    end
+    if product_type != ""
+      if truncate
+        "#{product_type}:<br/>#{product_name.truncate(30, :omission => "...")}".html_safe
+      else
+        "#{product_type}:<br/>#{product_name}".html_safe
+      end
+    else
+      if truncate
+        product_name.html_safe.truncate(30, :omission => "...")
+      else
+        product_name.html_safe
+      end
     end
   end
 
