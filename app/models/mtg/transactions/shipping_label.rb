1663bffa4a41c1b63e00b15f0c06ecba31a35be3
Ken <kvaczi2@gmail.com>
2012-12-29 14:10:30 -0600

    fixed bug where user enters blank first/last name in verified address causes empty name & stamp break when creating

--- app/models/mtg/transactions/shipping_label.rb
+++ app/models/mtg/transactions/shipping_label.rb
@@ -39,13 +39,15 @@ class Mtg::Transactions::ShippingLabel < ActiveRecord::Base
       paypal_address  = gateway.get_shipping_addresses(:pay_key => this_transaction.payment.paypal_paykey).selected_address
       
       #TODO: do we need to clean addresses coming from paypal?
-      self.to_address = { :full_name  =>  paypal_address.addressee_name || "#{this_transaction.buyer.account.first_name} #{this_transaction.buyer.account.last_name}", 
-                              :address1   =>  paypal_address.base_address[:line1], 
-                              :address2   =>  paypal_address.base_address[:line2], 
-                              :city       =>  paypal_address.base_address[:city], 
-                              :state      =>  paypal_address.base_address[:state],
-                              :country    =>  "US", #only allow US-based addresses for now
-                              :zip_code   =>  paypal_address.base_address[:postal_code] }
+      self.to_address = { :full_name  =>  paypal_address.addressee_name.blank? ? "#{this_transaction.buyer.account.first_name} #{this_transaction.buyer.account.last_name}" : paypal_address.addressee_name,
+                          :first_name =>  this_transaction.buyer.account.first_name,
+                          :last_name  =>  this_transaction.buyer.account.last_name,                          
+                          :address1   =>  paypal_address.base_address[:line1], 
+                          :address2   =>  paypal_address.base_address[:line2], 
+                          :city       =>  paypal_address.base_address[:city], 
+                          :state      =>  paypal_address.base_address[:state],
+                          :country    =>  "US", #only allow US-based addresses for now
+                          :zip_code   =>  paypal_address.base_address[:postal_code] }
                               
       #self.to_address = unconfirmed_address.merge!(:override_hash => Stamps.clean_address(:address => unconfirmed_address)[:address][:override_hash])
     end
