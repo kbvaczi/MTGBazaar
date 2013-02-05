3abb6e6abec9bf7f9416a489f85d529b8e8257f7
Ken <kvaczi2@gmail.com>
2013-02-05 03:32:35 -0600

    added background task to test for blitz

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,5 +12,13 @@ class HomeController < ApplicationController
   def blitz
     render :text => '42'
   end
+  
+  def test
+    sets      = Mtg::Set.pluck(:id)
+    set_id    = sets[rand(0...sets.length)]
+    card_type = card_type_list[rand(0...card_type_list.length)]
+    cards     = Mtg::Card.includes(:listings => :seller).where(:set_id => set_id, :card_type => card_type)    
+    Mtg::Cards::Statistics.delay.bulk_update_listing_information(cards.to_a.collect {|c| c.id})
+  end
 
 end
